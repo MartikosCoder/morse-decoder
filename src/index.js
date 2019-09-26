33c5d97e5f4c545204bc645f52a271053960acad
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2019-09-26 10:07:31 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const words = expr.split('**********');
+    const decoded_words = [];
+    words.forEach(word => {
+        const letters = [];
+        for(let i = 0; i < word.length / 10; i++) {
+            const un_wrapped_letter = word.slice(i * 10, (i + 1) * 10);
+            letters.push(un_wrapped_letter.slice(un_wrapped_letter.indexOf('1')));
+        }
+        decoded_words.push(letters
+            .map(elem => elem.replace(/10/g, '.'))
+            .map(elem => elem.replace(/11/g, '-'))
+            .map(elem => MORSE_TABLE[elem])
+            .join(''));
+    })
+
+    return decoded_words.join(' ');
 }
 
 module.exports = {
